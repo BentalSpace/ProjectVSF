76f87ba603d6206325d63402306eecda5362e93a
BentalSpace <wjddlsrl236@naver.com>
2023-06-05 23:10:24 +0900

    맵 생성 로직 변경(더 좋게, 막히는곳이 없도록 변경) git push origin master git status git status

--- Assets/2 Scripts/Door.cs
+++ Assets/2 Scripts/Door.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    public int roomNum;
+    public bool isRight;
+    public Vector3 targetPos;
+
     [SerializeField]
     GameObject door1;
     [SerializeField]
@@ -12,17 +16,36 @@ public class Door : MonoBehaviour
     [SerializeField]
     GameObject colliderPrefab;
 
+    public void Init(int _roomNum, bool _isRight)
+    {
+        roomNum = _roomNum;
+        isRight = _isRight;
+    }
     private void Start()
     {
-        float door1Dis = Mathf.Abs(door1.transform.position.y - transform.position.y);
-        float door2Dis = Mathf.Abs(door2.transform.position.y - transform.position.y);
+        //float door1Dis = Mathf.Abs(door1.transform.position.y - transform.position.y);
+        //float door2Dis = Mathf.Abs(door2.transform.position.y - transform.position.y);
 
-        GameObject obj = Instantiate(colliderPrefab);
-        obj.transform.position = transform.position + Vector3.up;
-        obj.transform.localScale = new Vector3(1, (door1Dis - 2) * -1, 1);
+        // 콜라이더 생성
 
-        obj = Instantiate(colliderPrefab);
-        obj.transform.position = transform.position + Vector3.down;
-        obj.transform.localScale = new Vector3(1, door2Dis - 2, 1);
+        //GameObject obj = Instantiate(colliderPrefab);
+        //obj.transform.position = transform.position + Vector3.up;
+        //obj.transform.localScale = new Vector3(1, (door1Dis - 2) * -1, 1);
+
+        //obj = Instantiate(colliderPrefab);
+        //obj.transform.position = transform.position + Vector3.down;
+        //obj.transform.localScale = new Vector3(1, door2Dis - 2, 1);
+    }
+
+    void InNextRoom()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            InNextRoom();
+        }
     }
 }
--- Assets/2 Scripts/MapCreator.cs
+++ Assets/2 Scripts/MapCreator.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -32,34 +31,140 @@ public class MapCreator : MonoBehaviour
     Dictionary<int, Map> mapInfo = new Dictionary<int, Map>();
 
     int nowMap = 0;
-    
 
+
+    void SetRoom(int mapNum, int roomNum, bool isThere = true)
+    {
+        mapInfo[mapNum].isRoom[roomNum] = isThere;
+
+        // 어떤 배치가 되어있는 방을 사용할 지
+        if (isThere)
+            mapInfo[mapNum].roomType[roomNum] = Random.Range(0, roomPrefab.Length);
+    }
     private void Start()
     {
-        // 방 배열 초기화
+        // Map 초기화
         for(int i = 0; i < mapCount; i++)
-        {
             mapInfo[i] = new Map(roomsPerFloor);
-            int useRoomNum = Random.Range(0, roomsPerFloor);
-            mapInfo[i].isRoom[useRoomNum] = true;
 
-            for(int j = 0; j < roomsPerFloor; j++)
+        // 방 배열 초기화
+        for (int i = 0; i < mapCount; i++)
+        {
+            if (i == 0)
             {
-                if (j == useRoomNum)
-                    continue;
-                float num = Random.Range(0, 10f);
-                if(num <= 2)
+                for (int j = 0; j < roomsPerFloor; j++)
                 {
-                    mapInfo[i].isRoom[j] = true;
-                    mapInfo[i].roomType[j] = Random.Range(0, roomPrefab.Length);
+                    // 첫번째 방이라면 다 생성
+                    SetRoom(i, j);
+                }
+            }
+
+            if (i != mapCount - 1)
+            {
+                int mapPlus1 = i + 1;
+                float rand;
+                for (int j = 0; j < roomsPerFloor; j++)
+                {
+                    if (!mapInfo[i].isRoom[j])
+                        continue;
+                    // ※ 현재 있는 방의 위치를 기준으로 앞에 있는 방을 생성하는거임
+                    if (j == 0)
+                    {
+                        // 0, 1 둘 중에 방 생성
+                        // 두개 다 생성할 지, 하나만 생성할 지(세개일때도 똑같음)
+                        rand = Random.Range(0, 10f);
+
+                        if (rand <= 1f)
+                        {
+                            // 두개 다 생성
+                            SetRoom(mapPlus1, 0);
+                            SetRoom(mapPlus1, 1);
+                        }
+                        else
+                        {
+                            // 둘 중 하나에만 생성
+                            rand = Random.Range(0, 2);
+
+                            if (rand == 0)
+                                SetRoom(mapPlus1, 0);
+                            else
+                                SetRoom(mapPlus1, 1);
+                        }
+                    }
+                    else if (j == roomsPerFloor - 1)
+                    {
+                        // roomsPerFloor -1, -2 둘 중에 방 생성
+                        rand = Random.Range(0, 10f);
+
+                        if (rand <= 1f)
+                        {
+                            // 두개 다 생성
+                            SetRoom(mapPlus1, roomsPerFloor - 1);
+                            SetRoom(mapPlus1, roomsPerFloor - 2);
+                        }
+                        else
+                        {
+                            // 둘 중 하나에만 생성
+                            rand = Random.Range(0, 2);
+                            if (rand == 0)
+                                SetRoom(mapPlus1, roomsPerFloor - 1);
+                            else
+                                SetRoom(mapPlus1, roomsPerFloor - 2);
+                        }
+                    }
+                    else
+                    {
+                        // 내가 있는 방 0, +1, -1 셋 중에 방 생성
+                        rand = Random.Range(0, 10f);
+                        if (rand <= 3f)
+                        {
+                            // 3곳 다 생성
+                            SetRoom(mapPlus1, j - 1);
+                            SetRoom(mapPlus1, j);
+                            SetRoom(mapPlus1, j + 1);
+                        }
+                        else if (rand <= 4f)
+                        {
+                            // 2곳 생성 (1 2 | 1 3 | 2 3)
+                            rand = Random.Range(0, 3);
+
+                            if (rand == 0)
+                            {
+                                SetRoom(mapPlus1, j - 1);
+                                SetRoom(mapPlus1, j);
+                            }
+                            else if(rand == 1)
+                            {
+                                SetRoom(mapPlus1, j);
+                                SetRoom(mapPlus1, j + 1);
+                            }
+                            else
+                            {
+                                SetRoom(mapPlus1, j - 1);
+                                SetRoom(mapPlus1, j + 1);
+                            }
+                        }
+                        else
+                        {
+                            // 1곳 생성 60% (1 2 3)
+                            rand = Random.Range(0, 3);
+
+                            if (rand == 0)
+                                SetRoom(mapPlus1, j - 1);
+                            else if (rand == 1)
+                                SetRoom(mapPlus1, j);
+                            else
+                                SetRoom(mapPlus1, j + 1);
+                        }
+                    }
                 }
             }
         }
 
         // 방 생성
-        for(int i = 0; i < mapCount; i++)
+        for (int i = 0; i < mapCount; i++)
         {
-            for(int j = 0; j < roomsPerFloor; j++)
+            for (int j = 0; j < roomsPerFloor; j++)
             {
                 if (mapInfo[i].isRoom[j])
                 {
@@ -71,7 +176,7 @@ public class MapCreator : MonoBehaviour
                     // 만들어야 함
 
                     // 다음 방
-                    if(i < mapCount - 1)
+                    if (i < mapCount - 1)
                     {
                         DoorCheck(i, j, obj.transform.position);
                     }
@@ -139,19 +244,21 @@ public class MapCreator : MonoBehaviour
         {
             if (mapInfo[mapNum].isRoom[_i])
             {
-                GameObject door = Instantiate(doorPrefab);
-                SetDoorPosition(pos, door.transform, _i - now, isRight);
+                GameObject obj = Instantiate(doorPrefab);
+                SetDoorPosition(pos, obj.transform, _i - now, isRight);
+                Door door = obj.GetComponent<Door>();
+                door.Init(_i, isRight);
             }
         }
     }
     void SetDoorPosition(Vector3 roomPos, Transform doorTr, int y, bool isRight)
     {
-        if(isRight)
+        if (isRight)
             doorTr.position = roomPos + (Vector3.right * 11.25f) + Vector3.up * y * 5;
         else
             doorTr.position = roomPos + (Vector3.left * 11.25f) + Vector3.up * y * 5;
     }
-    
+
     public void RoomSPawn()
     {
         GameObject obj = Instantiate(roomPrefab[mapInfo[nowMap].roomType[0]].gameObject);
